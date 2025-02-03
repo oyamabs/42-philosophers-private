eec2a0ef6480afa64cc1f78630f609073b819938
Theo Champion <tchampio@student.42lehavre.fr>
2025-02-03 18:05:11 +0100

    Added a first argument check

--- philo/philo.c
+++ philo/philo.c
@@ -6,7 +6,7 @@
 /*   By: freddy </var/mail/freddy>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/03 14:45:13 by freddy            #+#    #+#             */
-/*   Updated: 2025/02/03 17:12:35 by freddy           ###   ########.fr       */
+/*   Updated: 2025/02/03 18:04:49 by freddy           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -43,7 +43,62 @@ typedef struct s_params
 	pthread_mutex_t	meal_check;
 }	t_params;
 
+int	ft_atoi(const char *s)
+{
+	int	isnegative;
+	int	res;
+
+	res = 0;
+	isnegative = 1;
+	while (*s == ' ' || *s == '\f' || *s == '\r' || *s == '\v' || *s == '\t'
+		|| *s == '\n')
+		s++;
+	if (*s == '-' || *s == '+')
+	{
+		if (*s == '-')
+			isnegative = -1;
+		s++;
+	}
+	while (*s >= '0' && *s <= '9')
+	{
+		res = (res * 10) + (*s - '0');
+		s++;
+	}
+	return (res * isnegative);
+}
+
+bool	is_arg_digit(char *arg)
+{
+	int	i;
+
+	i = 0;
+	while (arg[i])
+	{
+		if (arg[i] < '0' || arg[i] > '9')
+			return (false);
+		i++;
+	}
+	return (true);
+}
+
+bool	check_args(int argc, char **argv)
+{
+	int	current_arg;
+
+	current_arg = 1;
+	if (argc < 5 && argc > 6)
+		return (false);
+	while (current_arg < argc)
+		if (!is_arg_digit(argv[current_arg]))
+			return (false);
+	return (true);
+}
+
 int	main(int argc, char **argv)
 {
+	if (check_args(argc, argv))
+		printf("Tout est bon chef\n");
+	else
+		printf("C'est la merde chef\n");
 	return (0);
 }
