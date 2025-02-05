044c666fd96d042f54d3c26c27cf3ebf4e569d20
Theo Champion <tchampio@student.42lehavre.fr>
2025-02-05 01:12:37 +0100

    Created initializers and various changes, described further
    - Changed includes to only include pthread.h as the library for threads
      (clang lsp imported a fucky one)
    - Added some fields in structs

--- philo/philo.c
+++ philo/philo.c
@@ -6,14 +6,14 @@
 /*   By: freddy </var/mail/freddy>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/03 14:45:13 by freddy            #+#    #+#             */
-/*   Updated: 2025/02/05 00:20:28 by freddy           ###   ########.fr       */
+/*   Updated: 2025/02/05 01:12:34 by freddy           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <bits/pthreadtypes.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
+#include <pthread.h>
 #define MAX_PHILOSOPHERS 1024
 
 typedef size_t	t_timestamp;
@@ -24,9 +24,10 @@ typedef struct s_philo
 	int	meals_count;
 	int	min_meals;
 	bool	is_eating;
+	bool	*is_dead;
 	t_timestamp	sleep_time;
 	t_timestamp	meal_time;
-	t_timestamp	thinking_time;
+	t_timestamp	death_time;
 	t_timestamp	start;
 	t_timestamp	last_meal;
 	pthread_mutex_t	*left_fork;
@@ -42,6 +43,8 @@ typedef struct s_params
 	pthread_mutex_t	write_check;
 	pthread_mutex_t	death_check;
 	pthread_mutex_t	meal_check;
+	t_philo	*philos;
+	int	philos_number;
 }	t_params;
 
 int	ft_atoi(const char *s)
@@ -104,6 +107,65 @@ bool	check_args(int argc, char **argv)
 	return (true);
 }
 
+void	create_forks(pthread_mutex_t *forks_arr, int forks)
+{
+	int	i;
+
+	i = 0;
+	while (i < forks)
+	{
+		pthread_mutex_init(&(forks_arr[i]), NULL);
+		i++;
+	}
+}
+
+void	init_timestamps_params(t_philo *philo, char **argv)
+{
+	philo->start = 0;
+	philo->last_meal = 0;
+	philo->meal_time = ft_atoi(argv[2]);
+	philo->sleep_time = ft_atoi(argv[3]);
+	philo->death_time = ft_atoi(argv[4]);
+	if (argv[5])
+		philo->min_meals = ft_atoi(argv[5]);
+}
+
+void	create_philos(t_params *params, t_philo *philos, pthread_mutex_t *forks, char **argv)
+{
+	int	i;
+
+	i = 0;
+	while (i < params->philos_number)
+	{
+		philos[i].id = i + 1;
+		philos[i].meals_count = 0;
+		init_timestamps_params(&philos[i], argv);
+		philos[i].is_eating = false;
+		philos[i].write_check = &params->write_check;
+		philos[i].meal_check = &params->meal_check;
+		philos[i].death_check = &params->death_check;
+		philos[i].is_dead = &params->dead;
+		philos[i].left_fork = &forks[i];
+		if (i == 0)
+			philos[i].right_fork = &forks[params->philos_number - 1];
+		else
+			philos[i].right_fork = &forks[i];
+		i++;
+	}
+}
+
+void	init_all(t_params *params, pthread_mutex_t *forks, t_philo *philos, int philosnum, char **argv)
+{
+	params->dead = false;
+	params->philos = philos;
+	params->philos_number = ft_atoi(argv[1]);
+	pthread_mutex_init(&(params->meal_check), NULL);
+	pthread_mutex_init(&(params->write_check), NULL);
+	pthread_mutex_init(&(params->death_check), NULL);
+	create_forks(forks, philosnum);
+	create_philos(params, philos, forks, argv);
+}
+
 int	main(int argc, char **argv)
 {
 	t_philo	philos[MAX_PHILOSOPHERS];
@@ -112,5 +174,6 @@ int	main(int argc, char **argv)
 
 	if (!check_args(argc, argv))
 		printf("Wrong usage: ./philo.out <philosophers> <time to die> <time to eat> <time to sleep> [min number of meals for philosphers]\n");
+	init_all(&params, forks, philos, ft_atoi(argv[1]), argv);
 	return (0);
 }
