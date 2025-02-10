aff7039f180f1ea8ad39d52a82de17b95fc75435
Theo Champion <tchampio@student.42lehavre.fr>
2025-02-10 16:57:06 +0100

    normed philo.c

--- philo/philo.c
+++ philo/philo.c
@@ -6,7 +6,7 @@
 /*   By: freddy </var/mail/freddy>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/03 14:45:13 by freddy            #+#    #+#             */
-/*   Updated: 2025/02/10 16:49:42 by freddy           ###   ########.fr       */
+/*   Updated: 2025/02/10 16:54:53 by freddy           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -40,7 +40,8 @@ bool	check_args(int argc, char **argv)
 		current_arg++;
 	}
 	if (ft_atoi(argv[1]) > MAX_PHILOSOPHERS || ft_atoi(argv[1]) <= 0
-		|| ft_atoi(argv[2]) <= 0 || ft_atoi(argv[3]) <= 0 || ft_atoi(argv[4]) <= 0)
+		|| ft_atoi(argv[2]) <= 0 || ft_atoi(argv[3]) <= 0
+		|| ft_atoi(argv[4]) <= 0)
 		return (false);
 	if (argc == 6)
 		if (ft_atoi(argv[5]) < 0)
@@ -50,13 +51,15 @@ bool	check_args(int argc, char **argv)
 
 int	main(int argc, char **argv)
 {
-	t_philo	philos[MAX_PHILOSOPHERS];
-	t_params	params;
+	t_philo			philos[MAX_PHILOSOPHERS];
+	t_params		params;
 	pthread_mutex_t	forks[MAX_PHILOSOPHERS];
 
 	if (!check_args(argc, argv))
 	{
-		printf("Wrong usage: ./philo.out <philosophers> <time to die> <time to eat> <time to sleep> [min number of meals for philosphers]\n");
+		printf("Wrong usage: ./philo.out <philosophers> <time to die>"
+			" <time to eat> <time to sleep> "
+			"[min number of meals for philosphers]\n");
 		return (1);
 	}
 	init_all(&params, forks, philos, ft_atoi(argv[1]), argv);
